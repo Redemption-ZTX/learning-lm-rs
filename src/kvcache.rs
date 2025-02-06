d6e9f0f50b882d53095c4faeec940d380b0e3aaa
Tianxiang Zhao <162714929+Redemption-ZTX@users.noreply.github.com>
2025-02-07 01:44:45 +0800

    Update kvcache.rs

--- src/kvcache.rs
+++ src/kvcache.rs
@@ -3,14 +3,15 @@ use std::{usize, vec};
 use crate::tensor::Tensor;
 pub struct KVCache<T> {
     k_cache: Vec<Tensor<T>>, // (max_seq_len, n_kv_head * dqkv) x layers
-    v_cache: Vec<Tensor<T>>, // (max_seq_len, n_kv_head * dqkv) x layers
+    v_cache: Vec<Tensor<T>>,
+    /// (max_seq_len, n_kv_head * dqkv) x layers
     #[allow(unused)]
     max_seq_len: usize,
     dim: usize,
     length: usize, // length of the current sequence
 }
 
-impl<T: Default + Copy> KVCache<T> {
+impl<T: Default + Copy + 'static> KVCache<T> {
     pub fn new(n_layers: usize, max_seq_len: usize, dim: usize, init_len: usize) -> Self {
         KVCache {
             k_cache: (0..n_layers)
@@ -33,7 +34,7 @@ impl<T: Default + Copy> KVCache<T> {
         self.v_cache[layer].slice(start * self.dim, &vec![self.length - start, self.dim])
     }
 
-    pub fn increment(&mut self, seq_len: usize){
+    pub fn increment(&mut self, seq_len: usize) {
         self.length += seq_len;
     }
 
